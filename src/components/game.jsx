fd5ebec6dbfe80a3ccab94886448ff121f90b1d1
deviantfero <fmorataya.04@gmail.com>
2017-11-13 01:20:33 -0600

    fix styling in root page

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -34,20 +34,20 @@ export default class Game extends React.Component {
   render() {
     let board = this.state.game ? <Board cols={this.state.rows} rows={this.state.rows}/> : null;
     return (
-      <div className="board-container">
-        <div className="col-md-12">
-          <h1 className="col-md-12 text-center">{this.state.rows} x {this.state.rows} Tic Tac Toe</h1>
-          <div className="col-md-6" style={{display: this.state.game ? 'none' : 'initial'}}>
-          <form action={"/play/" + this.state.rows} method="GET">
-              <input type="number" 
-                value={this.state.rows} 
-                onChange={this.updateBoard()}/>
-              <button 
-                type="submit" 
-                onClick={this.startGame}>
-                Start Game
-              </button>
-          </form>
+      <div className="parent-container">
+        <div className="game">
+          <div className="board-container">
+            <div>
+              <h1 className="text-center">{this.state.rows} x {this.state.rows} Tic Tac Toe</h1>
+              <div className="col-md-12" style={{display: this.state.game ? 'none' : 'initial'}}>
+              <form action={"/play/" + this.state.rows} method="GET">
+                  <input type="number" 
+                    className="text-center"
+                    value={this.state.rows} 
+                    onChange={this.updateBoard()}/>
+              </form>
+              </div>
+            </div>
           </div>
         </div>
       </div>
